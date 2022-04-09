4c114166ea664cd22b456661ea5155ac105d2c2d
Clinton wambugu <clintonwambugu@gmail.com>
2022-04-09 21:31:14 +0300

    Create quote class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from "./quote";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quotes:Quote[] = [
+    {id:1, name:"Ashes fly back into the face of him who throws them."},
+    {id:2, name:"Do not follow a person who is running away."},
+    {id:3, name:"He who is being carried does not realize how far the town is."},
+    {id:4, name:"A flea can trouble a lion more than a lion can trouble a flea."}
+  ]
 }
