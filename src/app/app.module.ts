9f3db1d624add5c43c527d73c8f018112cccfc1d
Clinton wambugu <clintonwambugu@gmail.com>
2022-04-10 16:09:55 +0300

    Create highest upvotes highlight directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { HighestUpvotesDirective } from './highest-upvotes.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteDetailComponent
+    QuoteDetailComponent,
+    HighestUpvotesDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/highest-upvotes.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appHighestUpvotes]'
+})
+export class HighestUpvotesDirective {
+
+  constructor(private elem:ElementRef) {
+    this.elem.nativeElement.style.backgroundColor = 'green';
+  }
+
+}
